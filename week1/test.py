8a6e9f66aa0a0192b629f1945491647a642fe7bf
Abraham Paniagua <abraham.paniagua@gmail.com>
2023-02-27 08:48:32 -0800

    ASDF

--- week1/test.py
+++ week1/test.py
@@ -1,22 +1,10 @@
-def crackle_pop(n: int = 100) -> None:
-    """
-    This function prints the numbers from 1 to n (inclusive). 
-    If the number is divisible by 3, it prints "Crackle" instead of the number.
-    If it's divisible by 5, it prints "Pop". 
-    If it's divisible by both 3 and 5, it prints "CracklePop".
+def maxSubArray(nums):
+    max_here = nums[0]
+    max_so_far = nums[0]
+    
+    for i in range(len(nums))
+    
 
-    :param n: The last number to print (inclusive). Default is 100.
-    :type n: int
-    :return: None
-    """
-    for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            print("CracklePop")
-        elif i % 3 == 0:
-            print("Crackle")
-        elif i % 5 == 0:
-            print("Pop")
-        else:
-            print(i)
-            
-crackle_pop(1)
+assert maxSubArray([-2,1,-3,4,-1,2,1,-5,4]) == 6
+assert maxSubArray([1]) == 1
+assert maxSubArray([5,4,-1,7,8]) == 23
